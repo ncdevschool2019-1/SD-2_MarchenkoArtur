1feca14d49311b01d01c86018660d6f9a8efbb42
Artur Marchenko <marchenko.mac@gmail.com>
2019-03-18 11:55:48 +0300

    A file was deleted

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var sign_in_button = document.getElementById("sign_in_button");
-
-    sign_in_button.addEventListener('click', function (event) {
-        event.preventDefault();
-        document.getElementById('autorisation').style.display = 'none';
-        document.getElementById('main_page').style.display = 'block';
-    })
-}
